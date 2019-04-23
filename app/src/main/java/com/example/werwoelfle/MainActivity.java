ba1a2626eb0d40f9088de0ee2580691a07b09370
Lucaraid <luca.wandfluh@gmail.com>
2019-04-23 16:40:49 +0200

    feat: add main and group signup page

--- app/src/main/java/com/example/werwoelfle/MainActivity.java
+++ app/src/main/java/com/example/werwoelfle/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.werwoelfle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Werewolf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.startseite);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "MainActivity: onResume()");
+        super.onResume();
+    }
+
+    public void next(View v) {
+        Log.d(TAG, "MainActivity: next()");
+        // Get Player Count
+        Spinner playerDropdown = (Spinner) findViewById(R.id.playerCount);
+        int players = Integer.parseInt(playerDropdown.getSelectedItem().toString());
+
+        // Create Group SignUp Intent
+        Intent groupSignUpActivityIntent = new Intent(this, GroupSignUpActivity.class);
+        groupSignUpActivityIntent.putExtra("players", players);
+
+        startActivity(groupSignUpActivityIntent);
     }
 }
