83862f60626154fcafbfb78e8abd41229919286e
Lucaraid <luca.wandfluh@gmail.com>
2019-04-29 17:27:23 +0200

    feat: add language support for role names

--- app/src/main/java/com/example/werwoelfle/Roles.java
+++ app/src/main/java/com/example/werwoelfle/Roles.java
@@ -1,10 +1,22 @@
 package com.example.werwoelfle;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 public enum Roles {
-    WERWOLF,
-    DORFBEWOHNER,
-    AMOR,
-    SEHER,
-    HEXE,
-    JAEGER,
+    WEREWOLF,
+    VILLAGER,
+    CUPID,
+    SEER,
+    WITCH,
+    HUNTER;
+
+    public String getLabel(Context context) {
+        Resources res = context.getResources();
+        int resId = res.getIdentifier(this.name(), "string", context.getPackageName());
+        if (0 != resId) {
+            return (res.getString(resId));
+        }
+        return (name());
+    }
 }
