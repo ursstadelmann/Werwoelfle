4e104a96709dc442baa0fa312280df8b10b2e1f1
Lucaraid <luca.wandfluh@gmail.com>
2019-05-12 21:18:06 +0200

    feat: add givePhoneBackToLeader

--- app/src/main/java/com/example/werwoelfle/GivePhoneToLeaderActivity.java
+++ app/src/main/java/com/example/werwoelfle/GivePhoneToLeaderActivity.java
@@ -1,6 +1,19 @@
 package com.example.werwoelfle;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class GivePhoneToLeaderActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_phone_back_to_leader);
+        }
+
+        public void next(View v) {
+            Intent nightGoingToSleep = new Intent(this, NightGoingToSleepActivity.class);
+            startActivity(nightGoingToSleep);
+        }
 }
